035c7c635fd9cb26ddeea81465961633080493c8
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-29 00:38:38 +0100

    two test commands

---
+++ scripts/cmd/parse-data/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("this will parse data in a text file into a JSON file")
+}
---
+++ scripts/go.mod
@@ -0,0 +1,3 @@
+module github.com/edwardtanguay/datapod-for-react-go-json
+
+go 1.22.1
