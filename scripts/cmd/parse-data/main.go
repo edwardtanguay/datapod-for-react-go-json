de087765b60d2ee36835d6dce6d1685ba6a2f612
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-29 02:13:56 +0100

    tools to utils

--- scripts/cmd/parse-data/main.go
+++ scripts/cmd/parse-data/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"datapod-for-react-go-json/cmd/tools"
+	"datapod-for-react-go-json/cmd/utils"
 	"fmt"
 )
 
 func main() {
 
-	tools.Devlog("this will parse the data")
-	lines := tools.GetLinesFromFile("../../../data/flashcards.txt")
+	utils.Devlog("this will parse the data")
+	lines := utils.GetLinesFromFile("../../../data/flashcards.txt")
 
 	for i, line := range lines {
 		fmt.Printf("%d: WORKS: %s\n", i+1, line)
--- scripts/cmd/utils/debug.go
+++ scripts/cmd/utils/debug.go
@@ -1,4 +1,4 @@
-package tools
+package utils
 
 import "fmt"
 
@@ -10,5 +10,5 @@ devlog("no files are locked")
 devlog(fmt.Sprintf("There are %d flashcards.", len(flashcards)))
 */
 func Devlog(line string) {
-	fmt.Printf("3>>> %s\n", line)
+	fmt.Printf("444>>> %s\n", line)
 }
