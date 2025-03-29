bfa89c46f39b6dcf6d1cbdfe3024bdb9d3a5ec4c
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-29 01:25:41 +0100

    showing text

--- scripts/cmd/parse-data/tools.go
+++ scripts/cmd/parse-data/tools.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func devlog(line string) {
 	fmt.Printf("2>>> %s\n", line)
+}
+
+/*
+Get all lines from a file as a slice of strings
+
+lines := getLinesFromFile("../../notes.txt")
+
+- use relative path
+*/
+func getLinesFromFile(fileName string) []string {
+	byteContents, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	contents := string(byteContents)
+	lines := strings.Split(contents, "\n")
+	for i, line := range lines {
+		lines[i] = strings.TrimSpace(line)
+	}
+	return lines
 }
