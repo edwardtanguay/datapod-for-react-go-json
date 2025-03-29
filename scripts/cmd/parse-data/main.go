48a5c2d829f97e6c216f72e0addc882d7335882b
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-29 01:36:45 +0100

    working with common file

--- scripts/cmd/parse-data/main.go
+++ scripts/cmd/parse-data/main.go
@@ -1,33 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
 
 	devlog("this will parse the data")
-	file, err := os.Open("../../../data/flashcards.txt")
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
+	lines := getLinesFromFile("../../../data/flashcards.txt")
 
 	for i, line := range lines {
-		fmt.Printf("%d: %s\n", i+1, line)
+		fmt.Printf("%d: WORKS: %s\n", i+1, line)
 	}
 }
