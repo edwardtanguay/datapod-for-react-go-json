161e48e83f60fb9da95a9735b83928dcd5d19139
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-22 18:51:40 +0200

    parameter works

--- scripts/cmd/cli-learn/main.go
+++ scripts/cmd/cli-learn/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"datapod-for-react-go-json/developer/learn"
+	"fmt"
+	"os"
 	// import "datapod-for-react-go-json/qtools/qcli"
 )
 
 func main() {
-	learn.Ex001()
-	learn.Ex002()
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <exerciseNumber>")
+		fmt.Println("Example: go run main.go 001")
+		return
+	}
+
+	exerciseNumber := os.Args[1]
+
+	switch exerciseNumber {
+	case "001":
+		learn.Ex001()
+	case "002":
+		learn.Ex002()
+	default:
+		fmt.Printf("Invalid exercise number: %s\n", exerciseNumber)
+	}
 }
